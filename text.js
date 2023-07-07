6292fda8d8f9652dc6f9fe9e999e808dc3d5d8e9
Zeki <Qotursuz@gmail.com>
2023-07-07 22:07:59 +0400

    projectev

---
+++ text.js
@@ -0,0 +1,10 @@
+let ucbucaq = alert('Ucbucaqin Perimetrini Tapin');
+let a = +prompt ('Ucbucaqin A terefini Qeyd Edin');
+let b = +prompt ('Ucbucaqin B terefini Qeyd Edin');
+let c = +prompt ('Ucbucaqin C terefini Qeyd Edin');
+
+let ucbucaqpr = a + b + c ;
+
+document.getElementById('dark').innerHTML =  a, ' ',  b, ' ', c,'Bunlar ucbucaqin Terefleridir.Bu ededlerin perimetri', ucbucaqpr,'olar';
+
+
